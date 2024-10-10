e32013ea667fdf2d243fcfde367ee14b28a0c3e3
Chris <cajcrook@gmail.com>
2024-10-10 12:13:43 +0100

    name in nav bar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,17 +1,43 @@
 // importing the logout componant
 import LogoutButton from "./LogoutButton";
+import NavUserName from "./NavUserName";
+import { useState, useEffect } from "react";
+import { getUserInfo } from "../services/user";
+import { useNavigate } from "react-router-dom";
 
 // importing the nav bar css
 //import "./NavBar.css"
-
 import { 
     Nav, 
     HomeLogo, 
     LogoContainer, 
-    Menu 
+    Menu, 
 } from './styles/NavBar.styled.js';
 
 function NavBar() {
+
+const [user, setUser] = useState('');
+const navigate = useNavigate();
+
+useEffect(() => {
+    const fetchData = async () => {
+      const token = localStorage.getItem("token");
+      const loggedIn = token !== null;
+      if (loggedIn) {
+        try {
+          const userData = await getUserInfo(token);
+          setUser(userData.userInfo[0]);
+        console.log(userData.userInfo[0].username)
+
+        } catch (err) {
+          console.log(err);
+          navigate("/login");
+        }
+      }
+    };
+    fetchData();
+  }, [navigate]);
+
     return (
         <Nav>
             <HomeLogo>
@@ -19,6 +45,11 @@ function NavBar() {
                     <LogoContainer>🍉</LogoContainer>
                 </a>
             </HomeLogo>
+            <NavUserName
+            // username={user.username}
+            firstName={user.firstName}
+            lastName={user.lastName}
+            />
             <Menu>
                 <a href="/profile">Profile</a>
                 <a href="/friends">Friends</a>
